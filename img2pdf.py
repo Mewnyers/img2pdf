fa61de5fe0b20791693a43542f57741800734787
tonton <satmsr918@gmail.com>
2023-05-02 19:01:11 +0900

    Support for major image files

--- img2pdf.py
+++ img2pdf.py
@@ -5,7 +5,7 @@ from PyPDF2 import PdfMerger
 
 # 引数の数を確認する
 if len(sys.argv) < 2:
-    print('Usage: python webp2pdf.py [folder_path_or_file_path]')
+    print('Usage: python img2pdf.py [folder_path_or_file_path]')
     sys.exit(1)
 
 # 引数からフォルダ名またはファイル名を取得する
@@ -13,10 +13,10 @@ input_path = sys.argv[1]
 
 # フォルダが指定された場合
 if os.path.isdir(input_path):
-    # .webpをすべて.pdfに変換する
+    # すべてのイメージファイルを.pdfに変換する
     for file_name in os.listdir(input_path):
-        if file_name.endswith('.webp'):
-            # PILで.webpを開く
+        if file_name.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff', '.webp')):
+            # PILでイメージファイルを開く
             img = Image.open(os.path.join(input_path, file_name))
             # .pdfに変換するために拡張子を変更
             pdf_path = os.path.join(input_path, os.path.splitext(file_name)[0] + '.pdf')
@@ -39,14 +39,17 @@ if os.path.isdir(input_path):
     for file_name in os.listdir(input_path):
         if file_name.endswith('.pdf'):
             pdf_path = os.path.join(input_path, file_name)
-            webp_path = os.path.join(input_path, os.path.splitext(file_name)[0] + '.webp')
-            if os.path.exists(webp_path):
-                os.remove(pdf_path)
+            image_ext = os.path.splitext(file_name)[0]
+            for ext in ('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff', '.webp'):
+                image_path = os.path.join(input_path, image_ext + ext)
+                if os.path.exists(image_path):
+                    os.remove(pdf_path)
+                    break
     print('Conversion complete!')
 
 # ファイルが指定された場合
-elif os.path.isfile(input_path) and input_path.endswith('.webp'):
-    # PILで.webpを開く
+elif os.path.isfile(input_path) and input_path.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff', '.webp')):
+    # PILでイメージファイルを開く
     img = Image.open(input_path)
     # .pdfに変換するために拡張子を変更
     pdf_path = os.path.splitext(input_path)[0] + '.pdf'
@@ -55,5 +58,5 @@ elif os.path.isfile(input_path) and input_path.endswith('.webp'):
     print('Conversion complete!')
 
 else:
-    print('Usage: python webp2pdf.py [folder_path_or_file_path]')
+    print('Usage: python img2pdf.py [folder_path_or_file_path]')
     sys.exit(1)
